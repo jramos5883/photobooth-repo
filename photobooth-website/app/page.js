2c77d879b6d31cf59dffcb9253e4dc578263d36a
jramos5883 <jramos5883@gmail.com>
2023-09-14 11:45:54 -0700

    Adding company logo images and setting up custom break points

--- photobooth-website/app/page.js
+++ photobooth-website/app/page.js
@@ -1,7 +1,10 @@
 export default function LandingPage() {
   return (
     <div className="bg-purple-300 h-screen">
-      <h1>360 Photo Booth! Landing Page!</h1>
+      <h1>Landing Page!</h1>
+      <div className="bg-blue-300 cellsm:bg-blue-600 celllg:bg-blue-900 tablet:bg-yellow-300 lapsm:bg-green-300 laplg:bg-green-600 desksm:bg-red-600 desklg:bg-red-900">
+        Background
+      </div>
     </div>
   );
 }
--- photobooth-website/tailwind.config.js
+++ photobooth-website/tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        // Custom Break Points
+        cellsm: "360px",
+        celllg: "412px",
+        tablet: "768px",
+        lapsm: "1024px",
+        laplg: "1366px",
+        desksm: "1920px",
+        desklg: "2560px",
       },
     },
   },
   plugins: [],
-}
+};
